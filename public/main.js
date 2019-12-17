f59ef3323cea74ed978a9aa00dd31f01793e76b6
Michelkazi <michel.m.kazi@gmail.com>
2019-12-17 02:57:12 -0500

    I decided to make everything yellow

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 let signupModal = document.getElementById('splash-text')
+let solicit = document.getElementById('solicit-phone-number')
 let sendMsg = document.getElementById('btn')
 let dismiss = document.getElementById('dismiss-btn')
 
@@ -6,8 +7,14 @@ sendMsg.addEventListener('click', (e) => {
     e.preventDefault()
     const name = document.getElementById('name-input')
     const input = document.getElementById('phone-number')
-    signupModal.style.display='none'
-    if (!input.value.includes('@')){
+    solicit.style.display = 'none'
+    signupModal.innerHTML =
+        `<h1>HI ${name.value.toUpperCase()}</h1>
+        <h1>WELCOME TO WAKE 'N' BAKERY</h1>
+        <h2>WE'RE YOUR QUALITY MEDICAL CANNABIS INFUSED PASTRY SHOP</h2>
+        `
+        
+        if (!input.value.includes('@')){
         (async () => {
             console.log(input.value)
             let form = new FormData()
