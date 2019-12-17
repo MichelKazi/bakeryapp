8e296ffe76623e43dbe11b2420c2619bbf549c9f
Michelkazi <michel.m.kazi@gmail.com>
2019-12-17 10:41:30 -0500

    More yellow less ugly?

--- public/main.js
+++ public/main.js
@@ -1,7 +1,11 @@
 let signupModal = document.getElementById('splash-text')
 let solicit = document.getElementById('solicit-phone-number')
 let sendMsg = document.getElementById('btn')
-let dismiss = document.getElementById('dismiss-btn')
+let logo = document.getElementById('logo')
+let navInfo = document.getElementById('nav-info')
+let intersect = document.getElementById('intersection-point')
+let nav = document.getElementById('navbar')
+let navMenu = document.getElementById('nav')
 
 sendMsg.addEventListener('click', (e) => {
     e.preventDefault()
@@ -27,7 +31,25 @@ sendMsg.addEventListener('click', (e) => {
     }
 })
 
-dismiss.addEventListener('click', (e) => {
-    e.preventDefault()
-    signupModal.style.display='none'
+let resizer = new IntersectionObserver((entries) => {
+    const [el] = entries
+    if (el.isIntersecting) {
+       
+           logo.style.transform = 'scale(.75)'
+           navInfo.style.borderBottom = ''
+            navInfo.style.height = '15.25vh'
+           nav.style.height = '18.75vh'
+           nav.style.transform = 'scale(1.25)'
+           
+           console.log("intersection point is in the viewport!");
+        } else {
+            
+            logo.style.transform = 'scale(.65)'
+            navInfo.style.height = '12.25vh'
+            nav.style.height = '15.75vh'
+            nav.style.transform = ''
+            
+        console.log("intersection point is not in the viewport!");
+    }
 })
+resizer.observe(intersect)
