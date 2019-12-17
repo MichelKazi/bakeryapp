ef809a9efb8e5eb5d1d9bd4b7a425874ba70ced8
Michelkazi <michel.m.kazi@gmail.com>
2019-12-17 05:31:35 -0500

    Super cool custom emails

--- server.rb
+++ server.rb
@@ -27,23 +27,23 @@ end
 
 
 post '/email' do
-  from = Email.new(email: 'test@example.com')
-  subject = 'Wake \'n\' Bakery Mailing List'
-  to = Email.new(email: params['user_email'])
-  content = Content.new(type: 'text/plain', value: "Thanks for signing up for email alerts from Wake 'n' Bakery")
-  mail = SendGrid::Mail.new(from, subject, to, content)
-  # puts JSON.pretty_generate(mail.to_json)
-  puts mail.to_json
+#   from = Email.new(email: 'test@example.com')
+#   subject = 'Wake \'n\' Bakery Mailing List'
+#   to = Email.new(email: params['user_email'])
+#   content = Content.new(type: 'text/plain', value: "Thanks for signing up for email alerts from Wake 'n' Bakery")
+#   mail = SendGrid::Mail.new(from, subject, to, content)
+#   # puts JSON.pretty_generate(mail.to_json)
+#   puts mail.to_json
 
-	# mail = SendGrid::Mail.new
-	# mail.template_id = 'd-c72f0db72da446d1ad8a3872c8cfde30' # a non-legacy template id
-	# mail.from = Email.new(email: 'michel.m.kazi@gmail.com')
-	# subject = 'Dynamic Template Data Hello World from the Twilio SendGrid Ruby Library'
-	# mail.subject = subject
-	# personalization = Personalization.new
-	# personalization.add_to(Email.new(email: params['user_email'], name: params['user_name']))
+	mail = SendGrid::Mail.new
+	mail.template_id = 'd-c72f0db72da446d1ad8a3872c8cfde30' # a non-legacy template id
+	mail.from = Email.new(email: 'michel.m.kazi@gmail.com')
+	subject = 'Dynamic Template Data Hello World from the Twilio SendGrid Ruby Library'
+	mail.subject = subject
+	personalization = Personalization.new
+	personalization.add_to(Email.new(email: params['user_email']))
 	
-	# mail.add_personalization(personalization)
+	mail.add_personalization(personalization)
 
 	
   sg = SendGrid::API.new(api_key: ENV['SENDGRID_AUTH'], host: 'https://api.sendgrid.com')
